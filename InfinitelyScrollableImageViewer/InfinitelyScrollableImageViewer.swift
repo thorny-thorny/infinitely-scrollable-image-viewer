b91b513e0897eb704d9c81d980a67043700ad1fb
thorny <35000252+thorny-thorny@users.noreply.github.com>
2024-09-18 16:22:14 +0200

    feat: basic pan gesture

--- InfinitelyScrollableImageViewer/InfinitelyScrollableImageViewer.swift
+++ InfinitelyScrollableImageViewer/InfinitelyScrollableImageViewer.swift
@@ -1,8 +1,32 @@
 import UIKit
 
 class InfinitelyScrollableImageViewer: UIView {
+    var baseOffset = CGPointZero
+    var offset = CGPointZero
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        
+        let panGr = UIPanGestureRecognizer(target: self, action: #selector(onPan))
+        
+        addGestureRecognizer(panGr)
+    }
+    
+    @objc func onPan(_ panGr: UIPanGestureRecognizer) {
+        let translation = panGr.translation(in: self)
+        
+        switch panGr.state {
+        case .possible, .began:
+            break
+        case .changed:
+            offset = CGPointMake(baseOffset.x + translation.x, baseOffset.y + translation.y)
+        case .ended, .cancelled, .failed:
+            baseOffset = offset
+        @unknown default:
+            break
+        }
+        
+        setNeedsDisplay()
     }
     
     override func draw(_ rect: CGRect) {
@@ -13,8 +37,8 @@ class InfinitelyScrollableImageViewer: UIView {
         let columns = Int(ceil(width / tileSize)) + 1
         let rows = Int(ceil(height / tileSize)) + 1
         
-        let centerTileX = rect.width / 2 - tileSize / 2
-        let centerTileY = rect.height / 2 - tileSize / 2
+        let centerTileX = rect.width / 2 - tileSize / 2 + offset.x
+        let centerTileY = rect.height / 2 - tileSize / 2 + offset.y
         
         let startX = centerTileX - tileSize * ceil(centerTileX / tileSize)
         let startY = centerTileY - tileSize * ceil(centerTileY / tileSize)
